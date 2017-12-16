edf8575db6139c3becf8ef77bd067317a720eff7
Kevin Chen <kchenpc@gmail.com>
2017-12-16 15:55:13 -0800

    We made a button changing a label

--- hello_ios/ViewController.swift
+++ hello_ios/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "Muffins!!!"
+    }
+    
+    
 }
 
