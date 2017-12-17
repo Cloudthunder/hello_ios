7826ba791ad9456fe2c045cb771353db0e36916e
Kevin Chen <kchenpc@gmail.com>
2017-12-17 14:48:05 -0800

    made a bad calculator

--- hello_ios/ViewController.swift
+++ hello_ios/ViewController.swift
@@ -10,23 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var counter: Int = 0
     
-    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func messageButtonPressed(_ sender: UIButton) {
-        messageLabel.text = "Muffins!!!"
-    }
     
+    
+    @IBAction func changingCounter(_ sender: UIButton) {
+        if sender.tag == 0 {
+            counter += 1
+        }
+        else if sender.tag == 1{
+            counter -= 1
+        }
+        else if sender.tag == 2{
+            counter *= 2
+        }
+        else if sender.tag == 3{
+            counter /= 2
+        }
+        counterLabel.text = "\(counter)"
+    }
+
     
 }
-
